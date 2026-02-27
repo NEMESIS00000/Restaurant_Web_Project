5b11517b7b9e69d2528530f7009bb8c461f21eb1
Your Name <you@example.com>
2026-02-27 13:08:29 +0530

    Ready for Render deployment

--- Resturant_Project/settings.py
+++ Resturant_Project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-local-dev-key"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", ".onrender.com"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
